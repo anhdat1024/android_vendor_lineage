1cc609bc8d38bde96e4380269af1b8330db72561
Karan Parashar <whyredfire@gmail.com>
2022-08-20 04:34:14 +0200

    vendor: bootanimation: Import from Pixel
    Change-Id: I6951a103b5ad4c0ba86b1989bd0ae4c121e580e6

--- config/common.mk
+++ config/common.mk
@@ -151,6 +151,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+#  Bootanimation
+include vendor/lineage/config/bootanimation.mk
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
