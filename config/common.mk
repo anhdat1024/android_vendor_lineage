2f683fa3a3e69ef678dc31752ea32e440d94ad5e
ghostrider-reborn <adithya.r02@outlook.com>
2025-10-14 19:52:36 +0800

    config: Disable dexpreopt minidebuginfo
    Change-Id: Ief843fc411189906c5d0183a6ff4485f6b3041ee

--- config/common.mk
+++ config/common.mk
@@ -128,6 +128,9 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
+# Disable dexpreopt minidebuginfo
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Lineage interfaces
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
